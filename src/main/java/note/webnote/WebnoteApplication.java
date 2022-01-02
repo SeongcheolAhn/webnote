007c67cef489f09400047dcc82895edd65b3989b
SeongcheolAhn <seongcheol.ahn@gmail.com>
2022-01-02 19:38:15 +0900

    Init

---
+++ src/main/java/note/webnote/WebnoteApplication.java
@@ -0,0 +1,13 @@
+package note.webnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/note/webnote/WebnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package note.webnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
