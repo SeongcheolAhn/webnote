6efc4ec0061e058a87bbdb67425cada38e712ecf
SeongcheolAhn <seongcheol.ahn@gmail.com>
2022-01-05 16:46:49 +0900

    refactor : MemberNote 리포, 서비스 삭제
    굳이 클래스로 만들 필요 X
    Member, Note 클래스의 리포, 서비스의 메소드로 있어도 충분하다.

--- src/main/java/note/webnote/web/controller/MemberController.java
+++ src/main/java/note/webnote/web/controller/MemberController.java
@@ -7,10 +7,7 @@ import note.webnote.web.form.MemberSaveForm;
 import note.webnote.web.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
