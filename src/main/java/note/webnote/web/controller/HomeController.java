6c2451c1bb3d2f02434e3f0e3554fbb8b4f60ed8
SeongcheolAhn <seongcheol.ahn@gmail.com>
2022-01-09 01:06:13 +0900

    fix : 로그인 한 사용자가 로그인 홈으로 가는 문제 해결

--- src/main/java/note/webnote/web/controller/HomeController.java
+++ src/main/java/note/webnote/web/controller/HomeController.java
@@ -1,13 +1,27 @@
 package note.webnote.web.controller;
 
+import note.webnote.web.intercptor.SessionMember;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String home(HttpServletRequest request) {
+
+        // 비로그인 사용자
+        HttpSession session = request.getSession();
+        if (session == null || session.getAttribute("LoginMember") == null) {
+            return "home";
+        }
+
+        SessionMember loginMember = (SessionMember) session.getAttribute("LoginMember");
+        Long memberId = loginMember.getId();
+
+        return "redirect:/members/" + memberId;
     }
 }
