ee56ea0ec53ebfa556666c61493368c196b27449
SeongcheolAhn <seongcheol.ahn@gmail.com>
2022-01-25 16:57:15 +0900

    test : HOST 권한 노트 조회 동적 쿼리 테스트 추가

--- src/test/java/note/webnote/QuerydslTest.java
+++ src/test/java/note/webnote/QuerydslTest.java
@@ -1,8 +1,11 @@
 package note.webnote;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import note.webnote.domain.Member;
-import note.webnote.domain.QMember;
+import note.webnote.domain.*;
+import note.webnote.repository.MemberRepository;
+import note.webnote.repository.NoteRepository;
+import note.webnote.web.dto.MemberHomeCondition;
+import note.webnote.web.dto.MemberHomeMemberNoteDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -17,6 +21,12 @@ public class QuerydslTest {
 
     @Autowired
     EntityManager em;
+
+    @Autowired
+    NoteRepository noteRepository;
+    @Autowired
+    MemberRepository memberRepository;
+
 
     @Test
     void test() {
@@ -34,5 +44,40 @@ public class QuerydslTest {
                 .fetchOne();
 
         Assertions.assertThat(result).isEqualTo(member);
+    }
+
+    /**
+     * 멤버의 전체 노트 조회시 동적 쿼리 동작 테스트
+     */
+    @Test
+    void memberHomeTest() {
+
+        // given
+        Member memberA = new Member("memberA", "loginA", "1234");
+        memberRepository.save(memberA);
+        Member memberB = new Member("memberB", "loginB", "1234");
+        memberRepository.save(memberB);
+        Member memberC = new Member("memberC", "loginC", "1234");
+        memberRepository.save(memberC);
+
+        Note note = new Note("titleA", "contentA");
+        noteRepository.save(note);
+
+        MemberNote memberNoteA = new MemberNote(memberA, note, Permission.HOST);
+        noteRepository.saveMemberNote(memberNoteA);
+        MemberNote memberNoteB = new MemberNote(memberB, note, Permission.READ_ONLY);
+        noteRepository.saveMemberNote(memberNoteB);
+        MemberNote memberNoteC = new MemberNote(memberC, note, Permission.READ_ONLY);
+        noteRepository.saveMemberNote(memberNoteC);
+
+        // when
+        MemberHomeCondition condition = new MemberHomeCondition();
+        condition.setPermission("HOST");
+
+        List<MemberHomeMemberNoteDto> result = noteRepository.findMemberNoteDto(memberA.getId(), condition);
+
+        // then
+        Assertions.assertThat(result.size()).isEqualTo(1);
+
     }
 }
