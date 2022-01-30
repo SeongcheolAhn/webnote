746c32c909a78e5fa7751491e68d661b50ce82a1
SeongcheolAhn <seongcheol.ahn@gmail.com>
2022-01-30 14:16:35 +0900

    chore : 배포 자동화 구성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,4 +32,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+    -Dspring.config.location=/home/ec2-user/app/step2/zip/application.yml \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
